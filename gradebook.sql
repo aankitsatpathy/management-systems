01b73da624e158891ee22d85b1bbbbcd7d59cfee
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-12-16 11:41:05 +0530

    Create gradebook.sql

---
+++ gradebook.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE school;
+
+CREATE TABLE student(
+    names varchar(200),
+    roll_number int,
+    maths int,
+    physics int,
+    chemistry int,
+    biology int,
+    english int,
+    percent float(20),
+)
